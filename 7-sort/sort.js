e9373d04c9e48462c6728dba3163afe2c331ae8a
steam <jizhengqistrongman@126.com>
2019-10-30 09:34:22 +0800

    add sort

--- 7-sort/sort.js
+++ 7-sort/sort.js
@@ -8,8 +8,23 @@ function bubbleSort(arr) {
   }
 }
 
-function insertionSort() {
-  //
+function insertionSort(arr) {
+  if(arr.length <= 1) return;
+
+  for(let i = 0; i < arr.length; i++) {
+    let value = arr[i];
+    let j = i - 1;
+
+    for(; j >= 0; --j) {
+      if(arr[j] > value) {
+        arr[j+1] = arr[j]; // 后移一位
+      } else { // 已经找到合适的位置了，不再遍历左侧已排序数组
+        break;
+      };
+    };
+
+    arr[j+1] = value; // 插入数据
+  };
 }
 
 function selectionSort() {
