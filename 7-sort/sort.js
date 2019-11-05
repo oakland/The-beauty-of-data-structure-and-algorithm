e8ca61a3eabb77a5ea42e016f7cc3528a5e86632
steam <jizhengqistrongman@126.com>
2019-11-05 16:28:26 +0800

    selection sort simple optimize

--- 7-sort/sort.js
+++ 7-sort/sort.js
@@ -56,3 +56,19 @@ function selectionSort(arr) {
   }
 }
 
+function selectionSort(arr) {
+  for (let i = 0; i < arr.length; i++) {
+    let min = i;
+    for (let j = i + 1; j < arr.length; j++) {
+      if (arr[j] < arr[min]) {
+        min = j;
+      }
+    }
+    if(min !== i) {
+      let temp = arr[i];
+      arr[i] = arr[min]
+      arr[min] = temp;
+    }
+  }
+  return arr;
+}
